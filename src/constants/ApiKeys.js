ef26c09d439eb1b52853970a1be8599069ccda88
olalliot <olalliot@umich.edu>
2020-04-14 18:41:46 -0400

    gitignore update

---
+++ src/constants/ApiKeys.js
@@ -0,0 +1,12 @@
+export default {
+  FirebaseConfig : {
+    apiKey: "AIzaSyAEF3LZEibbtPQibiCCmjhMiNmBaATm97Q",
+    authDomain: "mileout-53149.firebaseapp.com",
+    databaseURL: "https://mileout-53149.firebaseio.com",
+    projectId: "mileout-53149",
+    storageBucket: "mileout-53149.appspot.com",
+    messagingSenderId: "794048177964",
+    appId: "1:794048177964:web:84d429cc9b9c5c5ee9ae89",
+    measurementId: "G-TVESCHYVN5"
+  }
+}
